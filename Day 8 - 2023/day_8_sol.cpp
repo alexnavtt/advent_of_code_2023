5efec132d6b7d7ca5b8b9520a7a1168c93214600
Alex Navarro <alexnavtt@utexas.edu>
2023-12-08 13:15:18 -0600

    Made it more concise

--- Day 8 - 2023/day_8_sol.cpp
+++ Day 8 - 2023/day_8_sol.cpp
@@ -6,6 +6,7 @@
 #include <functional>
 #include <unordered_set>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std::string_view_literals;
 
@@ -31,40 +32,27 @@ int main(){
     }
 
     // Make walk function so it can be reused for part 2
-    auto walkToEnd = [&desert_map, &instructions](std::string start, bool(*test_fn)(std::string)) -> size_t {
+    auto walkToEnd = [&desert_map, &instructions](std::string current, std::function<bool(const std::string&)> termination) -> size_t {
         size_t num_steps = 0;
-        std::string current = start;
         for (char instruction : std::views::repeat(instructions) | std::views::join){
-            if (test_fn(current)) break;
-
-            switch (instruction){
-                case 'R':
-                    current = desert_map.at(current).second;
-                    break;
-
-                case 'L':
-                    current = desert_map.at(current).first;
-                    break;
-
-                default:
-                    throw std::runtime_error("Unknown instruction!");
-            }
+            if (termination(current)) break;
+            auto [left, right] = desert_map.at(current);
+            current = (instruction == 'L' ? left : right);
             num_steps++;
         }
         return num_steps;
     }; 
-    size_t num_steps = walkToEnd("AAA", [](std::string s){return s == "ZZZ";});
+    size_t num_steps = walkToEnd("AAA", [](const std::string& s){return s == "ZZZ";});
     std::println("Took {} steps", num_steps);
 
     // Problem 2 solution
-    auto endsWithA = [](std::string s){return s.ends_with("A");};
-    auto endsWithZ = [](std::string s){return s.ends_with("Z");};
+    auto endsWithA = [](const std::string& s){return s.ends_with("A");};
+    auto endsWithZ = [](const std::string& s){return s.ends_with("Z");};
 
     size_t walk_steps = 1;
     for (auto start_point : desert_map | std::views::keys | std::views::filter(endsWithA)){
         walk_steps = std::lcm(walk_steps, walkToEnd(start_point, endsWithZ));
     }
-
     std::println("Minimum steps is {}", walk_steps);
 
     return 0;
