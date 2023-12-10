0295fe0dd6e592bc0f1d435479e5967f2093dade
Alex Navarro <alexnavtt@utexas.edu>
2023-12-10 11:05:00 -0600

    Cleanup

--- Day 10 - 2023/day_10_sol.cpp
+++ Day 10 - 2023/day_10_sol.cpp
@@ -21,11 +21,8 @@ struct Index{
     int x = 0;
     int y = 0;
 
-    bool outOfBounds(int max_x, int max_y){
-        return x < 0 
-            || x >= max_x
-            || y < 0
-            || y >= max_y;
+    bool outOfBounds(int max_x, int max_y) const{
+        return x < 0 || x >= max_x|| y < 0|| y >= max_y;
     }
 
     bool operator!=(const Index& other) const{
@@ -41,7 +38,7 @@ struct Index{
     }
 };
 
-std::unordered_map<Dir, Index> directions{
+static const std::unordered_map<Dir, Index> directions{
     {NORTH, { 0, -1}},
     {SOUTH, { 0,  1}},
     {EAST , { 1,  0}},
@@ -77,14 +74,22 @@ std::optional<Dir> getNext(char symbol, Dir last_move){
     return {};
 }
 
+enum DirectionChange : char{
+    NONE            = '0',
+    RIGHT           = '1',
+    LEFT            = '2',
+    NONE_HORIZONTAL = '3',
+    NONE_VERTICAL   = '4'
+};
+
 // Label actions based on how they turn a path moving northward
 static const std::unordered_map<char, char> remap{
-    {'F', '1'},  // Turns to the right
-    {'L', '1'},  // Turns to the right
-    {'7', '2'},  // Turns to the left
-    {'J', '2'},  // Turns to the left
-    {'-', '3'},  // Does not turn (horizontal)
-    {'|', '4'}   // Does not turn (vertical)
+    {'F', DirectionChange::RIGHT},
+    {'L', DirectionChange::RIGHT},
+    {'7', DirectionChange::LEFT},
+    {'J', DirectionChange::LEFT},
+    {'-', DirectionChange::NONE_HORIZONTAL},
+    {'|', DirectionChange::NONE_VERTICAL}
 };
 
 // To be inside, a point cast northwards must cross the path an odd number of times.
@@ -96,23 +101,26 @@ static const std::unordered_map<char, char> remap{
 bool isInside(Index idx, const std::vector<std::span<char>>& grid){
     // Cast a ray upwards and see how many times we cross the loop
     size_t num_intersections = 0;
-    char last_change = 'x';
+    char last_change = NONE;
     for (; idx.y >= 0; idx.y--){
         char test_char = grid[idx.y][idx.x];
         switch (test_char){
-        case '1':
-            if (last_change == '2') {num_intersections++; last_change = 'x';}
-            else if (last_change == '1') last_change = 'x';
-            else last_change = '1';
+        case RIGHT:
+            if (last_change == LEFT) {num_intersections++; last_change = NONE;}
+            else if (last_change == RIGHT) last_change = NONE;
+            else last_change = RIGHT;
             break;
-        case '2':
-            if (last_change == '1') {num_intersections++; last_change = 'x';}
-            else if (last_change == '2') last_change = 'x';
-            else last_change = '2';
+        case LEFT:
+            if (last_change == RIGHT) {num_intersections++; last_change = NONE;}
+            else if (last_change == LEFT) last_change = NONE;
+            else last_change = LEFT;
             break;
-        case '3':
+        case NONE_HORIZONTAL:
             num_intersections++;
             break;
+        case NONE:
+        case NONE_VERTICAL:
+            break; // Do nothing in this case
         }
     }
     return num_intersections % 2;
@@ -134,34 +142,51 @@ int main(){
     }
     std::println("Starting at index ({}, {})", start.x, start.y);
 
-    bool first_found = true;
+    // Find the two starting pipes from S
+    bool first_found = false;
     Dir path1_step, path2_step;
-    for (Dir dir : {NORTH, SOUTH, WEST, EAST}){
-        Index next = start + directions.at(dir);
+    for (const Dir dir : {NORTH, SOUTH, EAST, WEST}){
+        const Index next = start + directions.at(dir);
         if (next.outOfBounds(grid.front().size(), grid.size())) continue;
 
+        // Check to see if this character gives a valid move from S
         std::optional<Dir> first_move = getNext(grid[next.y][next.x], dir);
-        if (first_move && first_found){
+        if (first_move && !first_found){
             path1_step = dir;
-            first_found = false;
-        }else if (first_move && !first_found){
+            first_found = true;
+        }else if (first_move && first_found){
             path2_step = dir;
             break;
         }
     }
 
+    // Determine the pipe type of S for part 2
+    char& start_char = grid[start.y][start.x];
+    if (path1_step == NORTH){
+        switch (path2_step){
+            case SOUTH: start_char = NONE_VERTICAL; break;
+            case EAST : start_char = RIGHT; break;
+            case WEST : start_char = LEFT; break;
+        }
+    }else if (path1_step == SOUTH){
+        switch (path2_step){
+            case EAST : start_char = RIGHT; break;
+            case WEST : start_char = LEFT; break;
+        }
+    }else{
+        start_char = NONE_HORIZONTAL;
+    }
+
     int64_t steps = 0;
-    for (Index idx1 = start, idx2 = start; (idx1 != idx2) || (steps == 0); ){
-        idx1 = idx1 + directions[path1_step];
-        idx2 = idx2 + directions[path2_step];
+    for (Index idx1 = start, idx2 = start; (idx1 != idx2) || (steps == 0); steps++){
+        idx1 = idx1 + directions.at(path1_step);
+        idx2 = idx2 + directions.at(path2_step);
 
         char& c1 = grid[idx1.y][idx1.x];
         char& c2 = grid[idx2.y][idx2.x];
 
         path1_step = getNext(c1, path1_step).value();
         path2_step = getNext(c2, path2_step).value();
-
-        steps++;
 
         // Part 2
         if (remap.contains(c1)) c1 = remap.at(c1);
@@ -171,7 +196,7 @@ int main(){
     size_t num_inside = 0;
     for (auto [y, line] : grid | std::views::enumerate){
         for (auto [x, c] : line | std::views::enumerate){
-            if (c >= '1' && c <= '4' || c == 'S') continue;
+            if (c >= '1' && c <= '4') continue;
             if (isInside(Index{(int)x, (int)y}, grid)) {c = 'X'; num_inside++;}
             else c = 'O';
         }
