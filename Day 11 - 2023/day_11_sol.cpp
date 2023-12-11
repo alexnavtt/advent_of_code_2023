dffe1f63f86e611370df4d1518c0fac07e7ff32a
Alex Navarro <alexnavtt@utexas.edu>
2023-12-11 11:30:54 -0600

    Removed manual math in favor of ranges functions

--- Day 11 - 2023/day_11_sol.cpp
+++ Day 11 - 2023/day_11_sol.cpp
@@ -12,10 +12,6 @@ struct Index{
     int y;
 };
 
-size_t numGalaxies(auto range){
-    return std::ranges::distance(range | std::views::filter([](char c){return c == '#';}));
-}
-
 int main(){
     std::string input_str = loadInput("day_11_data.txt");
     const auto start_time = std::chrono::steady_clock::now();
@@ -28,7 +24,7 @@ int main(){
     std::vector<size_t> empty_col_indices;
     for (auto [y_idx, row] : input_str | std::views::split('\n') | std::views::enumerate){
         // Check if this row has no galaxies
-        if (numGalaxies(row) == 0) empty_row_indices.push_back(y_idx);
+        if (!std::ranges::contains(row, '#')) {empty_row_indices.push_back(y_idx); continue;}
         
         // Get the indices of all '#' characters
         auto galaxy_positions = row 
@@ -38,27 +34,29 @@ int main(){
             | std::views::transform([y_idx](auto x_idx){return Index{(int)x_idx, (int)y_idx};});
         
         // Append these to the existing list of indices
-        galaxy_indices.insert(std::end(galaxy_indices), std::ranges::begin(galaxy_positions), std::ranges::end(galaxy_positions));
+        std::ranges::copy(galaxy_positions, std::back_inserter(galaxy_indices));
     }
     for (auto col_idx : std::views::iota(size_t(0), line_length)){
-        // Construct a column view
+        // Construct a column view and check to see if it has any galaxies
         auto col = input_str | std::views::drop(col_idx) | std::views::stride(line_length+1);
-        // Check to see if this column has no galaxies
-        if (numGalaxies(col) == 0) empty_col_indices.push_back(col_idx);
+        if (!std::ranges::contains(col, '#')) empty_col_indices.push_back(col_idx);
     }
 
     size_t total_dist = 0;
     for (auto [gal_idx, g1] : galaxy_indices | std::views::enumerate){
         for (auto g2 : galaxy_indices | std::views::drop(gal_idx+1)){
 
-            auto extra_x = empty_col_indices 
-                | std::views::filter([x1 = g1.x, x2 = g2.x](size_t idx){
-                    return (idx >= x1 && idx <= x2) || (idx <= x1 && idx >= x2);
+            const size_t& xmin = std::min(g1.x, g2.x);
+            const size_t& xmax = std::max(g1.x, g2.x);
+            const size_t& ymin = std::min(g1.y, g2.y);
+            const size_t& ymax = std::max(g1.y, g2.y);
+
+            auto extra_x = empty_col_indices | std::views::filter([xmin,xmax](size_t idx){
+                    return idx == std::clamp<size_t>(idx, xmin, xmax);
                 });
 
-            auto extra_y = empty_row_indices
-                | std::views::filter([y1 = g1.y, y2 = g2.y](size_t idx){
-                    return (idx > y1 && idx < y2) || (idx < y1 && idx > y2);
+            auto extra_y = empty_row_indices | std::views::filter([ymin, ymax](size_t idx){
+                    return idx == std::clamp<size_t>(idx, ymin, ymax);
                 });
             
             size_t dist = std::abs(g1.x - g2.x) + std::abs(g1.y - g2.y) + 999999*std::ranges::distance(extra_x) + 999999*std::ranges::distance(extra_y);
